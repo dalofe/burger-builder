0dc85410eb31fb12867b7815ae1f63aec327eaca
zgdlopez <dlopez@kooomo.com>
2020-06-27 09:43:50 +0200

    U17 - 301 - Preparing the folders structure

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 import { connect } from 'react-redux';
-import * as actionType from '../../store/actions';
+import * as actionType from '../../store/actions/actionTypes';
 
 
 class BurgerBuilder extends Component {
